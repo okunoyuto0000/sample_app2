2e9256673fc444234e0aad2dec2924aad8ee31c1
okunoyuto0000 <okunoyuto0000@gmail.com>
2021-03-24 13:47:10 +0000

    [finish] 7章

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'dbfbcb6c3b310084ff24c8285f13528de4b266894103160f7a1ad19501b390d2180d402a62a64b43957832ceca8f435bc32a14f13bf5113466d0cc6f427e936e'
