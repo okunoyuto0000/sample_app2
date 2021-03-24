b0af07b593175edc94cc28555db4fede0cb243fc
okunoyuto0000 <okunoyuto0000@gmail.com>
2021-03-24 08:05:21 +0000

    [finish] 4章

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
+  get 'todolists/new' => 'todolists#new'
+  post 'todolists' => 'todolists#create'
+
 end
